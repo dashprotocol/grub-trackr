769a597aa73450ad9de8aeb27983dc5f1c22f82e
Victor Tu <46986265+dashprotocol@users.noreply.github.com>
2020-03-02 03:53:05 -0800

    Enable connection to mongo database with mongoose

---
+++ database/db.js
@@ -0,0 +1,49 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/grubTrackr', {useNewUrlParser: true});
+
+var db = mongoose.connection;
+
+var grubSchema = new mongoose.Schema({
+  item: String,
+  quantity: Number,
+  category: String,
+  expiration: String,
+  location: String
+});
+
+var Grub = mongoose.model('Grub', grubSchema);
+
+
+module.exports = {
+  save: (grub, callback) => {
+    console.log(grub);
+    var record = new Grub ({
+      item: grub.item,
+      quantity: grub.quantity,
+      category: grub.category,
+      expiration: grub.expiration,
+      location: grub.location
+    });
+
+    record.save(function (err, record) {
+      if (err) {
+        console.log('save error');
+        callback(err);
+      } else {
+        console.log('save success');
+        callback(null, record);
+      }
+    })
+  },
+  readAll: (location, callback) => {
+    Grub.find({ location: location }, function (err, grubs) {
+      if (err) {
+        console.log('ReadAll error');
+        callback(err);
+      } else {
+        console.log('ReadAll success');
+        callback(null, grubs);
+      }
+    })
+  }
+}
