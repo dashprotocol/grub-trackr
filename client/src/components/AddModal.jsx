231e5aba5b11089de7b63877f51a97457d8e2fe0
Victor Tu <46986265+dashprotocol@users.noreply.github.com>
2020-03-02 01:07:41 -0800

    Add location input for modal

--- client/src/components/AddModal.jsx
+++ client/src/components/AddModal.jsx
@@ -6,9 +6,31 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+
 
 
 class AddModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grubName: '',
+      quantity: 0,
+      location: '',
+    }
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+  }
+
+  handleLocationChange(event) {
+    this.setState({
+      location: event.target.value
+    });
+  }
+
   render() {
     const { classes } = this.props;
     return(
@@ -16,18 +38,41 @@ class AddModal extends Component {
       <Dialog open={this.props.open} onClose={this.props.handleClose} aria-labelledby="form-dialog-title">
         <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We will send updates
-            occasionally.
-          </DialogContentText>
           <TextField
             autoFocus
             margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
+            id="grub name"
+            label="New food item"
+            type="text"
             fullWidth
           />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="quantity"
+            label="quantity"
+            type="number"
+            fullWidth
+          />
+          <InputLabel id='location-dialog-select-label'>location</InputLabel>
+          <Select
+            labelId='location'
+            id='location'
+            value={this.state.location}
+            onChange={this.handleLocationChange}
+            input={<Input />}
+            fullWidth
+          >
+            <MenuItem value='Fridge'>
+              Fridge
+            </MenuItem>
+            <MenuItem value='Freezer'>
+              Freezer
+            </MenuItem>
+            <MenuItem value='Pantry'>
+              Pantry
+            </MenuItem>
+          </Select>
         </DialogContent>
         <DialogActions>
           <Button onClick={this.props.handleClose} color="primary">
